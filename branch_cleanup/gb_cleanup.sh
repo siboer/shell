f847cbb2d455858179f525c3d087255ab9e807b4
igor <ikuplevich@fitbit.com>
2014-09-17 16:27:42 -0700

    branch cleanup script as a function

--- branch_cleanup/gb_cleanup.sh
+++ branch_cleanup/gb_cleanup.sh
@@ -1,2 +1,12 @@
 #!/bin/bash
 git branch | sed "s/^ *//" | grep -v master | xargs git branch -D
+
+function gb_cleanup(){
+git checkout master        #in case you forgot to switch to master
+git branch |               #get all branches
+ sed "s/^ *//" |           #remove leading spaces
+   grep -v master |        #master should not be deleted!
+    xargs git branch -D    #delete branches
+printf "git branch\n%s\n" "$(git branch)"
+}
+
